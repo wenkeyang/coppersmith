ee0a31aac042ee3d75a090487c058715ffab32bb
Tin Pavlinic <tin@simplemachines.com.au>
2015-08-04 10:33:15 +1000

    joins

--- lifted/core/src/main/scala/au/com/cba/omnia/dataproducts/features/Join.scala
+++ lifted/core/src/main/scala/au/com/cba/omnia/dataproducts/features/Join.scala
@@ -11,23 +11,23 @@ object Join {
 
   class IncompleteJoin[L,R] {
     //Write as many of these as we need...
-    def on(cond: (L,R)=> Boolean): Joined[L, R] =
-      Joined[L,R](cond)
+    def on[J : Ordering](l: L => J, r: R => J): Joined[L, R, J] = Joined[L, R, J](l,r)
 
-
-    def on[P1,P2](cond: (P1, P2, R) => JoinCondition)(implicit e: (P1,P2)=:=L): Joined[(P1, P2), R] =
-      Joined((t1: (P1, P2), p3:R) => cond(t1._1, t1._2, p3))
-
-    def on[P1,P2,P3](cond: (P1, P2, P3, R) => JoinCondition)(implicit e: ((P1, P2), P3) =:= L): Joined[(P1, P2, P3), R] =
-      Joined((t1: (P1, P2, P3), r:R) => cond(t1._1, t1._2, t1._3, r))
-
-    def on[P1,P2,P3,P4](cond: (P1, P2, P3, P4,  R) => JoinCondition)(implicit e: (((P1, P2), P3), P4) =:= L): Joined[(P1, P2, P3, P4), R] =
-      Joined((t1: (P1, P2, P3, P4), r:R) => cond(t1._1, t1._2, t1._3, t1._4, r))
+//    TODO: Fix multi-arity
+//    def on[P1,P2](cond: (P1, P2, R) => JoinCondition)(implicit e: (P1,P2)=:=L): Joined[(P1, P2), R] =
+//      Joined((t1: (P1, P2), p3:R) => cond(t1._1, t1._2, p3))
+//
+//    def on[P1,P2,P3](cond: (P1, P2, P3, R) => JoinCondition)(implicit e: ((P1, P2), P3) =:= L): Joined[(P1, P2, P3), R] =
+//      Joined((t1: (P1, P2, P3), r:R) => cond(t1._1, t1._2, t1._3, r))
+//
+//    def on[P1,P2,P3,P4](cond: (P1, P2, P3, P4,  R) => JoinCondition)(implicit e: (((P1, P2), P3), P4) =:= L): Joined[(P1, P2, P3, P4), R] =
+//      Joined((t1: (P1, P2, P3, P4), r:R) => cond(t1._1, t1._2, t1._3, t1._4, r))
   }
 
-  case class Joined[L,R](cond: (L,R) => JoinCondition) extends JoinableTo[(L,R)]
+  case class Joined[L, R, J : Ordering](left: L => J, right: R => J) extends JoinableTo[(L,R)]
 
   def join[T]:JoinableTo[T] = new EmptyJoinableTo[T]
+  def apply[T] = join[T]
 }
 
 
@@ -40,8 +40,8 @@ object JoinDemo {
   case class HomeLoan(id:Long, accountId:Long, primaryCustomer: Long)
 
   join[Customer]
-    .to[Account].on(_.id === _.customerId)
-    .to[HomeLoan].on((customer:Customer, acct:Account, loan:HomeLoan) => loan.accountId === acct.id)
-    .to[Customer].on((customer:Customer, acct:Account, loan:HomeLoan, primaryCustomer:Customer) => loan.primaryCustomer === primaryCustomer.id)
+    .to[Account].on(_.id, _.customerId)
+//    .to[HomeLoan].on((customer:Customer, acct:Account, loan:HomeLoan) => loan.accountId === acct.id)
+//    .to[Customer].on((customer:Customer, acct:Account, loan:HomeLoan, primaryCustomer:Customer) => loan.primaryCustomer === primaryCustomer.id)
 
 }
