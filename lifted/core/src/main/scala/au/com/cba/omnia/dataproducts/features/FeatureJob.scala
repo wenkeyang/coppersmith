1e831376beb2392ebbe40a913fbf38e317a5556e
Kristian Domagala <krislistmail+git@gmail.com>
2015-08-11 16:01:06 +1000

    Attempt at generalising groupBy to a FeatureSource for aggregation feature sets, but put on hold. Differentiate concrete source types (new SourceConfiguration trait) from abstract to delineate functionality. Move RichJoined binding to FeatureSource file

--- lifted/core/src/main/scala/au/com/cba/omnia/dataproducts/features/FeatureJob.scala
+++ lifted/core/src/main/scala/au/com/cba/omnia/dataproducts/features/FeatureJob.scala
@@ -9,44 +9,15 @@ import au.com.cba.omnia.maestro.api._
 
 import au.com.cba.omnia.etl.util.SimpleMaestroJob
 
-import lift.scalding._
-
-import Join.Joined
-
 trait FeatureJobConfig {
   def sourcePath:  Path
   def featureSink: FeatureSink
 }
 
-object SimpleFeatureJob {
-
-  case class JoinedFeatureSource[L, R, J : Ordering](
-                                           j: Joined[L, R, J],
-                                           fmt: (FeatureSource[L], FeatureSource[R]),
-                                           filter: ((L, R)) => Boolean =  (in: (L, R)) => true) extends FeatureSource[(L, R)] {
-    def filter(p: ((L, R)) => Boolean): FeatureSource[(L, R)] = copy(filter = s => filter(s) && p(s))
-
-    def load(conf: FeatureJobConfig): Execution[TypedPipe[(L, R)]] = {
-      val (leftSrc, rightSrc) = fmt
-      for {
-        leftPipe <- leftSrc.load(conf)
-        rightPipe <- rightSrc.load(conf)
-        joinedPipe = liftJoin(j)(leftPipe, rightPipe)
-      } yield joinedPipe.filter(filter)
-    }
-  }
-
-  implicit class RichJoined[L, R, J: Ordering](j: Joined[L, R, J]) {
-    def asSource(fmt: (FeatureSource[L], FeatureSource[R]),
-                 filter: ((L, R)) => Boolean = (in: (L, R)) => true): FeatureSource[(L, R)] =
-      JoinedFeatureSource(j, fmt, filter)
-  }
-}
-
 abstract class SimpleFeatureJob extends SimpleMaestroJob {
   def generate[S1, S2, J](
     cfg:      Config => FeatureJobConfig,
-    source:   FeatureSource[(Feature.EntityId, Iterable[(S1, S2)])],
+    source:   FeatureSource[(S1, S2)],
     features: AggregationFeatureSet[(S1, S2)]
   ) = for {
       conf     <- Execution.getConfig.map(cfg)
--- lifted/core/src/main/scala/au/com/cba/omnia/dataproducts/features/FeatureSource.scala
+++ lifted/core/src/main/scala/au/com/cba/omnia/dataproducts/features/FeatureSource.scala
@@ -9,18 +9,63 @@ import au.com.cba.omnia.maestro.api._
 
 import au.com.cba.omnia.etl.util.{ParseUtils, SimpleMaestroJob}
 
+import lift.scalding._
+
+import Join.Joined
+
+object FeatureSource {
+  implicit class RichJoined[L, R, J: Ordering](j: Joined[L, R, J]) {
+    def bind(cfg: (SourceConfiguration[L], SourceConfiguration[R]),
+             filter: ((L, R)) => Boolean = (in: (L, R)) => true): FeatureSource[(L, R)] =
+      JoinedFeatureSource(j, cfg, filter)
+  }
+}
+
 trait FeatureSource[S] {
   def filter(p: S => Boolean): FeatureSource[S]
   def load(conf: FeatureJobConfig): Execution[TypedPipe[S]]
-  def groupBy[T](f: S => T): FeatureSource[(T, Iterable[S])] = ???
 }
 
+case class JoinedFeatureSource[L, R, J : Ordering](
+  j: Joined[L, R, J],
+  srcCfg: (SourceConfiguration[L], SourceConfiguration[R]),
+  filter: ((L, R)) => Boolean =  (in: (L, R)) => true
+) extends FeatureSource[(L, R)] {
+  // TODO: Not specific to Joined sources - lift up
+  def filter(p: ((L, R)) => Boolean): FeatureSource[(L, R)] = copy(filter = s => filter(s) && p(s))
+
+  def load(conf: FeatureJobConfig): Execution[TypedPipe[(L, R)]] = {
+    val (leftSrc, rightSrc) = srcCfg
+    for {
+      leftPipe <- leftSrc.load(conf)
+      rightPipe <- rightSrc.load(conf)
+      joinedPipe = liftJoin(j)(leftPipe, rightPipe)
+    } yield joinedPipe.filter(filter)
+  }
+}
+
+// FIXME: Needs further abstraction of underlying 'TypedPipe' structure, as load would
+// return a Grouped instance
+/*
+case class GroupedFeatureSource[S](
+  underlying: FeatureSource[S],
+  grouping: S => Feature.EntityId
+) extends FeatureSource[(Feature.EntityId, Iterable[S])] {
+  def load(conf: FeatureJobConfig): Execution[TypedPipe[(Feature.EntityId, Iterable[S])]] = {
+    underlying.load(conf).map(_.groupBy(grouping))
+  }
+}
+*/
+
+trait SourceConfiguration[S] {
+  def load(conf: FeatureJobConfig): Execution[TypedPipe[S]]
+}
 
 case class HiveTextSource[S <: ThriftStruct : Decode, P](
   tableName: String,
   partition: Partition[S, P],
   filter: S => Boolean = (_: S) => true
-) extends FeatureSource[S] {
+) extends SourceConfiguration[S] {
   def filter(f: S => Boolean): HiveTextSource[S, P] = copy(filter = (s: S) => filter(s) && f(s))
   def load(conf: FeatureJobConfig) = Execution.from {
     // FIXME: This implementation completely ignores errors
