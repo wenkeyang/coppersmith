693f246fa81199f5871f6b55967daecf66f668b8
Kristian Domagala <krislistmail+git@gmail.com>
2015-08-17 09:23:01 +1000

    Support generation of regular (not aggregation) feature sets

--- lifted/core/src/main/scala/au/com/cba/omnia/dataproducts/features/FeatureJob.scala
+++ lifted/core/src/main/scala/au/com/cba/omnia/dataproducts/features/FeatureJob.scala
@@ -1,7 +1,5 @@
 package au.com.cba.omnia.dataproducts.features
 
-import org.apache.hadoop.fs.Path
-
 import com.twitter.scalding.{Config, Execution}
 import com.twitter.scalding.typed._
 
@@ -17,22 +15,38 @@ trait FeatureJobConfig[S] {
 }
 
 abstract class SimpleFeatureJob extends SimpleMaestroJob {
+  def generate[S](cfg:      Config => FeatureJobConfig[S],
+                  features: FeatureSet[S]): Execution[JobStatus] =
+    generate[S](cfg, generateOneToMany(features)_)
+
+  def generate[S](cfg:      Config => FeatureJobConfig[S],
+                  features: AggregationFeatureSet[S]): Execution[JobStatus] =
+    generate[S](cfg, generateAggregate(features)_)
+
   def generate[S](
     cfg:      Config => FeatureJobConfig[S],
-    features: AggregationFeatureSet[S]
-  ): Execution[JobStatus] = for {
+    transform: TypedPipe[S] => TypedPipe[FeatureValue[_, _]]
+  ): Execution[JobStatus] = {
+    for {
       conf     <- Execution.getConfig.map(cfg)
       source    = conf.featureSource
       input    <- source.load(conf)
-      grouped   = input.groupBy(s => (features.entity(s), features.time(s)))
-      values    = generate(grouped, features)
+      values    = transform(input)
       _        <- conf.featureSink.write(values, conf)
     } yield JobFinished
+  }
+
+  private def generateOneToMany[S](features: FeatureSet[S])
+                                  (input: TypedPipe[S]): TypedPipe[FeatureValue[_, _]] =
+    input.flatMap(features.generate(_))
 
   // FIXME: Should be able to take advantage of shapless' tuple support in combination with
   // Aggregator.join in order to run the aggregators in one pass over the input
-  private def generate[S](grouped: Grouped[(EntityId, Time), S],
-                          features: AggregationFeatureSet[S]): TypedPipe[FeatureValue[_, _]] = {
+  private def generateAggregate[S](features: AggregationFeatureSet[S])
+                                  (input: TypedPipe[S]): TypedPipe[FeatureValue[_, _]] = {
+
+    val grouped: Grouped[(EntityId, Time), S] = input.groupBy(s => (features.entity(s), features.time(s)))
+
     features.aggregationFeatures.map(feature => {
       grouped.aggregate(feature.aggregator).toTypedPipe.map { case ((e, t), v) =>
         // FIXME: Change FeatureValue to take name (or metadata) instead of related feature
