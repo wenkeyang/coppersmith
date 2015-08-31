476e40b42caa233cf615e28a9a8b0b66b7547380
Tin Pavlinic <tin@simplemachines.com.au>
2015-08-31 11:45:27 +1000

    better types

--- examples/src/main/scala/examples/LegacyFeatureSetExample.scala
+++ examples/src/main/scala/examples/LegacyFeatureSetExample.scala
@@ -8,8 +8,8 @@ import Patterns.empty
  * An example of defining metadata for features that are defined elsewhere.
  */
 object LegacyFeatureSetExample extends MetadataSet {
-  val legacyFeature1 = empty(FeatureMetadata("cep_features", "lgc_ftr_1", "A string feature", Categorical))
-  val legacyFeature2 = empty(FeatureMetadata("cep_features", "lgc_ftr_2", "A decimal feature", Continuous))
+  val legacyFeature1 = empty(FeatureMetadata[Value.Str]    ("cep_features", "lgc_ftr_1", "A string feature", Categorical))
+  val legacyFeature2 = empty(FeatureMetadata[Value.Decimal]("cep_features", "lgc_ftr_2", "A decimal feature", Continuous))
 
   def features = List(legacyFeature1, legacyFeature2)
 }
