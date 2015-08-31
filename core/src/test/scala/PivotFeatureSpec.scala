25fd36064357f0511da67ca329ffdeed3312ee6f
Tin Pavlinic <tin@simplemachines.com.au>
2015-08-31 15:56:12 +1000

    No nothing feature types

--- core/src/test/scala/PivotFeatureSpec.scala
+++ core/src/test/scala/PivotFeatureSpec.scala
@@ -55,12 +55,24 @@ object PivotFeatureSetSpec extends Specification with ScalaCheck { def is = s2""
     )
   }
 
+
+
   def generateMetadataCompareMacro = {
-    val macroMetadata = CustomerFeatureSet.metadata.toSeq.map(_.copy(description = ""))
+    def copyNoDesc(oldMetadata: FeatureMetadata[Value]): FeatureMetadata[Value] = {
+      FeatureMetadata[Value](
+        namespace   = oldMetadata.namespace,
+        name        = oldMetadata.name,
+        description = "",
+        featureType = oldMetadata.featureType
+      )
+    }
+    val macroMetadata = CustomerFeatureSet.metadata.toSeq.map(copyNoDesc)
+
     val metadata = PivotMacro.pivotThrift[Customer](
       CustomerFeatureSet.namespace,
       CustomerFeatureSet.entity,
-      CustomerFeatureSet.time).features.map(_.metadata.copy(description = ""))
+      CustomerFeatureSet.time
+    ).features.map(_.metadata).map(copyNoDesc _)
 
 
 
