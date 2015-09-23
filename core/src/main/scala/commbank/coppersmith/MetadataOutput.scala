4309ba09b0edbf59b20241fff108900929ae7607
Tin Pavlinic <tin@simplemachines.com.au>
2015-09-23 11:54:39 +1000

    Fix metadata output to include sources

--- core/src/main/scala/commbank/coppersmith/MetadataOutput.scala
+++ core/src/main/scala/commbank/coppersmith/MetadataOutput.scala
@@ -1,9 +1,10 @@
 package commbank.coppersmith
 
 import commbank.coppersmith.Feature.Value
+import Feature._
 
 object MetadataOutput {
-  type MetadataPrinter = FeatureMetadata[Feature.Value] => String
+  type MetadataPrinter = Metadata[_, Feature.Value] => String
 
   val HydroPsv: MetadataPrinter = md => md.asHydroPsv
   val Markdown: MetadataPrinter = md =>
@@ -14,23 +15,25 @@ object MetadataOutput {
        |
        | ${md.description}
        |
+       | Source: ${md.sourceTag.tpe}
+       |
        | Feature type: ${md.featureType}
        | Value Type: ${md.valueType}
        |
      """.stripMargin
-  trait HasMetadata {
-    def metadata: Iterable[FeatureMetadata[Value]]
+  trait HasMetadata[S] {
+    def metadata: Iterable[Metadata[S, Value]]
   }
 
-  implicit def fromFeature[S, V <: Value](f:Feature[S, V]): HasMetadata = new HasMetadata {
-    def metadata: Iterable[FeatureMetadata[V]] = Seq(f.metadata)
+  implicit def fromFeature[S, V <: Value](f:Feature[S, V]): HasMetadata[S] = new HasMetadata[S] {
+    def metadata: Iterable[Metadata[S, V]] = Seq(f.metadata)
   }
 
-  implicit def fromMetadataSet(mds: MetadataSet) = new HasMetadata {
+  implicit def fromMetadataSet[S](mds: MetadataSet[S]) = new HasMetadata[S] {
     def metadata = mds.metadata
   }
 
-  def metadataString[V <: Value](md: HasMetadata, printer: MetadataPrinter): String = {
+  def metadataString[S, V <: Value](md: HasMetadata[S], printer: MetadataPrinter): String = {
     md.metadata.map(printer(_)).mkString("\n")
   }
 }
