45695ebb25f27401d83bbb3988115de5c64ce91c
Kristian Domagala <krislistmail+git@gmail.com>
2015-10-19 12:26:31 +1100

    Remove dependency on coppersmith and scalding

--- core/src/main/scala/commbank/coppersmith/tools/CoppersmithBootstrap.scala
+++ core/src/main/scala/commbank/coppersmith/tools/CoppersmithBootstrap.scala
@@ -7,17 +7,13 @@ import java.io.{File, FileInputStream, PrintStream}
 
 import scalaz._, Scalaz._
 
-import com.twitter.scalding.Args
-
-import Feature.{Description, Metadata, Name, Namespace}, Metadata.ValueType, ValueType._
-
 /*
  * Tool for generating blank feature scala code from an existing metadata PSV file.
  *
  * Supports the following formats
  *
  *   namespace.name|valueType|featureType
- *   namespace.name|description|valueType|featureType
+ *   namespace.name,valueType,featureType,description
  *
  * where valueType is one of "double", "int" or "string" and featureType is one of
  * "continuous" or "categorical"
@@ -29,12 +25,11 @@ import Feature.{Description, Metadata, Name, Namespace}, Metadata.ValueType, Val
  * If --out is missing, result is printed to stdout
  */
 object CoppersmithBootstrap {
-
   def main(rawArgs: Array[String]): Unit = {
-    val args       = Args(rawArgs)
-    val sourceType = args("source-type")
-    val inFile     = new File(args("file"))
-    val outFile    = args.optional("out").map(new File(_))
+    val args       = parseArgs(rawArgs)
+    val sourceType = args.getOrElse("source-type", sys.error("source-type arg missing"))
+    val inFile     = new File(args.getOrElse("file", sys.error("file arg missing")))
+    val outFile    = args.get("out").map(new File(_))
 
     val out = outFile.map(new PrintStream(_))
     try {
@@ -50,6 +45,12 @@ object CoppersmithBootstrap {
     }
   }
 
+  def parseArgs(args: Array[String]): Map[String, String] =
+    args.take(2) match {
+      case Array(n, v) if n.startsWith("--") => Map(n.drop(2) -> v) ++ parseArgs(args.drop(2))
+      case _                                 => Map()
+    }
+
   def run(sourceType: String, inFile: File, out: Option[PrintStream]) = {
     if (!inFile.exists) {
       s"File not found: ${inFile.getAbsolutePath}".failure.toValidationNel
@@ -64,31 +65,41 @@ object CoppersmithBootstrap {
     }
   }
 
-  type Psv = (Namespace, Name, Description, ValueType, Feature.Type)
+  type Namespace   = String
+  type Name        = String
+  type ValueType   = String
+  type FeatureType = String
+  type Description = String
+  type Metadata    = (Namespace, Name, ValueType, FeatureType, Description)
 
   def bootstrapScala(sourceType: String, psvMetadata: Source): ValidationNel[String, String] = {
-    val featureMetadata: ValidationNel[String, List[Psv]] =
+    val featureMetadata: ValidationNel[String, List[Metadata]] =
       psvMetadata.getLines.map(_.trim).zipWithIndex.filterNot { case (l, _) =>
         l.isEmpty || l.startsWith("#")
       }.toList.map {
-        case (line, idx) => parsePsv(line).leftMap(e => s"Error at line ${idx + 1}: $e").toValidationNel
-      }.sequence[({type l[a]=ValidationNel[String, a]})#l, Psv]
+        case (line, idx) => parseMetadata(line).leftMap(e => s"Error at line ${idx + 1}: $e").toValidationNel
+      }.sequence[({type l[a]=ValidationNel[String, a]})#l, Metadata]
 
     featureMetadata.map(toScala(sourceType))
   }
 
-  def parsePsv(s: String): Validation[String, Psv] = {
-    val parts = s.split('|') match {
-      case Array(qName,       vTypeStr, fTypeStr) => (qName, None,       vTypeStr, fTypeStr).success
-      case Array(qName, desc, vTypeStr, fTypeStr) => (qName, Some(desc), vTypeStr, fTypeStr).success
-      case _ => s"Could not parse pipe separated values from '$s'".failure
+  def parseMetadata(s: String): Validation[String, Metadata] =
+    parseMetadata(s, '|').right.getOrElse(parseMetadata(s, ','))
+
+  def parseMetadata(s: String, sep: Char): Validation[String, Metadata] = {
+    val values = s.split(sep)
+    val parts = values.take(3) match {
+      case Array(qName, vTypeStr, fTypeStr) => (qName, vTypeStr, fTypeStr).success
+      case _ => s"Could not parse separated values from '$s'".failure
     }
 
     import scalaz.Validation.FlatMap._
-    parts.flatMap { case (qualifiedName, oDesc, vTypeStr, fTypeStr) =>
+    parts.flatMap { case (qualifiedName, vTypeStr, fTypeStr) =>
       (parseName(qualifiedName) |@| parseTypes(vTypeStr, fTypeStr)) {
-        case ((ns, name), (vType, fType)) =>
-          (ns, name, oDesc.getOrElse(s"Description for $name"), vType, fType)
+        case ((ns, name), (vType, fType)) => {
+          val desc = if (values.size == 4) values(3) else s"Description for $name"
+          (ns, name, vType, fType, desc)
+        }
       }
     }
   }
@@ -106,27 +117,27 @@ object CoppersmithBootstrap {
   }.validation
 
   def parseValueType(vTypeStr: String) = vTypeStr.toLowerCase match {
-      case "double" => DecimalType.success
-      case "int"    => IntegralType.success
-      case "string" => StringType.success
+      case "double" => "DecimalType".success
+      case "int"    => "IntegralType".success
+      case "string" => "StringType".success
       case _        => s"Unknown value type '$vTypeStr'".failure
     }
 
-  def parseFeatureType(fTypeStr: String, vType: ValueType) = fTypeStr.toLowerCase match {
+  def parseFeatureType(fTypeStr: String, vType: String) = fTypeStr.toLowerCase match {
       case "continuous" => vType match {
-        case DecimalType  => Feature.Type.Continuous.success
-        case IntegralType => Feature.Type.Discrete.success
-        case _            => s"Invalid value type 'vType' for continuous feature".failure
+        case "DecimalType"  => "Continuous".success
+        case "IntegralType" => "Discrete".success
+        case _              => s"Invalid value type 'vType' for continuous feature".failure
       }
       case "categorical" => vType match {
-        case IntegralType => Feature.Type.Ordinal.success
-        case StringType   => Feature.Type.Nominal.success
-        case _            => s"Invalid value type 'vType' for categorical feature".failure
+        case "IntegralType" => "Ordinal".success
+        case "StringType"   => "Nominal".success
+        case _              => s"Invalid value type 'vType' for categorical feature".failure
       }
       case _ => s"Unknown feature type '$fTypeStr'".failure
    }
 
-  def toScala(sourceType: String)(metadata: Iterable[Psv]) = s"""
+  def toScala(sourceType: String)(metadata: Iterable[Metadata]) = s"""
 import commbank.coppersmith._
 import Feature._
 import Type._
@@ -134,7 +145,7 @@ import Type._
 trait $sourceType
 
 object ${sourceType}FeatureSet extends MetadataSet[$sourceType] {
-${metadata.map{ case (ns, name, desc, vType, fType) =>
+${metadata.map{ case (ns, name, vType, fType, desc) =>
 s"""  val ${camelCase(name)} = Metadata[$sourceType, Value.$vType](
       "$ns", "$name", "$desc", $fType
   )"""
