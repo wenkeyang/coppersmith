dada5650556120629e28e2a7cf42d20a36317e3f
Kristian Domagala <krislistmail+git@gmail.com>
2015-10-19 14:57:23 +1100

    Remove scalaz dependency and determine separator char from filename

--- core/src/main/scala/commbank/coppersmith/tools/CoppersmithBootstrap.scala
+++ core/src/main/scala/commbank/coppersmith/tools/CoppersmithBootstrap.scala
@@ -5,15 +5,14 @@ import scala.io.Source
 
 import java.io.{File, FileInputStream, PrintStream}
 
-import scalaz._, Scalaz._
-
 /*
- * Tool for generating blank feature scala code from an existing metadata PSV file.
+ * Tool for generating blank feature scala code from an existing metadata file. Supports
+ * both PSV and CSV (based on file name extension).
  *
  * Supports the following formats
  *
  *   namespace.name|valueType|featureType
- *   namespace.name,valueType,featureType,description
+ *   namespace.name|valueType|featureType|description
  *
  * where valueType is one of "double", "int" or "string" and featureType is one of
  * "continuous" or "categorical"
@@ -31,11 +30,17 @@ object CoppersmithBootstrap {
     val inFile     = new File(args.getOrElse("file", sys.error("file arg missing")))
     val outFile    = args.get("out").map(new File(_))
 
-    val out = outFile.map(new PrintStream(_))
+    val sep = inFile.getName.reverse.takeWhile(_ != '.').reverse.toLowerCase match {
+      case "csv" => ','
+      case "psv" => '|'
+      case x     => sys.error(s"Unsupported file extension '$x'")
+    }
+
+    lazy val out = outFile.map(new PrintStream(_))
     try {
-      run(sourceType, inFile, out).fold(
+      run(sourceType, inFile, sep, out).fold(
         e => {
-          e.foreach(System.err.println)
+          System.err.println(e)
           System.exit(-1)
         },
         _ => System.exit(0)
@@ -51,14 +56,14 @@ object CoppersmithBootstrap {
       case _                                 => Map()
     }
 
-  def run(sourceType: String, inFile: File, out: Option[PrintStream]) = {
+  def run(sourceType: String, inFile: File, sep: Char, out: => Option[PrintStream]) = {
     if (!inFile.exists) {
-      s"File not found: ${inFile.getAbsolutePath}".failure.toValidationNel
+      Left(s"File not found: ${inFile.getAbsolutePath}")
     } else {
       val fileIn = new FileInputStream(inFile)
       val source = Source.fromInputStream(fileIn)
       try {
-        bootstrapScala(sourceType, source).map(out.getOrElse(System.out).println(_))
+        bootstrapScala(sourceType, source, sep).right.map(out.getOrElse(System.out).println(_))
       } finally {
         fileIn.close
       }
@@ -72,69 +77,71 @@ object CoppersmithBootstrap {
   type Description = String
   type Metadata    = (Namespace, Name, ValueType, FeatureType, Description)
 
-  def bootstrapScala(sourceType: String, psvMetadata: Source): ValidationNel[String, String] = {
-    val featureMetadata: ValidationNel[String, List[Metadata]] =
-      psvMetadata.getLines.map(_.trim).zipWithIndex.filterNot { case (l, _) =>
+  def bootstrapScala(sourceType: String, metadata: Source, sep: Char): Either[String, String] = {
+    val featureMetadata: Either[String, List[Metadata]] =
+      metadata.getLines.map(_.trim).zipWithIndex.filterNot { case (l, _) =>
         l.isEmpty || l.startsWith("#")
-      }.toList.map {
-        case (line, idx) => parseMetadata(line).leftMap(e => s"Error at line ${idx + 1}: $e").toValidationNel
-      }.sequence[({type l[a]=ValidationNel[String, a]})#l, Metadata]
+      }.toList.foldLeft[Either[String, List[Metadata]]](Right(List())) {
+        case (r, (line, idx)) => r match {
+          case e@Left(_) => e
+          case Right(ms) => parseMetadata(line, sep).fold(
+            e => Left(s"Error at line ${idx + 1}: $e"),
+            m => Right(m :: ms)
+          )
+        }
+      }
 
-    featureMetadata.map(toScala(sourceType))
+    featureMetadata.right.map(toScala(sourceType))
   }
 
-  def parseMetadata(s: String): Validation[String, Metadata] =
-    parseMetadata(s, '|').right.getOrElse(parseMetadata(s, ','))
-
-  def parseMetadata(s: String, sep: Char): Validation[String, Metadata] = {
+  def parseMetadata(s: String, sep: Char): Either[String, Metadata] = {
     val values = s.split(sep)
-    val parts = values.take(3) match {
-      case Array(qName, vTypeStr, fTypeStr) => (qName, vTypeStr, fTypeStr).success
-      case _ => s"Could not parse separated values from '$s'".failure
+    val parts: Either[String, (String, String, String)] = values.toList.take(3) match {
+      case List(qName, vTypeStr, fTypeStr) => Right((qName, vTypeStr, fTypeStr))
+      case _                               => Left(s"Could not parse separated values from '$s'")
     }
 
-    import scalaz.Validation.FlatMap._
-    parts.flatMap { case (qualifiedName, vTypeStr, fTypeStr) =>
-      (parseName(qualifiedName) |@| parseTypes(vTypeStr, fTypeStr)) {
-        case ((ns, name), (vType, fType)) => {
+    parts.right.flatMap { case (qName, vTypeStr, fTypeStr) =>
+      parseName(qName).right.flatMap { case (namespace, name) =>
+        parseTypes(vTypeStr, fTypeStr).right.map { case (vType, fType) => {
           val desc = if (values.size == 4) values(3) else s"Description for $name"
-          (ns, name, vType, fType, desc)
-        }
+          (namespace, name, vType, fType, desc)
+        }}
       }
     }
   }
 
-  def parseName(qName: String) = qName.split('.') match {
-      case Array(ns, name) => ((ns, name)).success
-      case _               => s"Could not parse name from '$qName'".failure
+  def parseName(qName: String): Either[String, (String, String)] = qName.split('.') match {
+      case Array(ns, name) => Right((ns, name))
+      case _               => Left(s"Could not parse name from '$qName'")
     }
 
   def parseTypes(vTypeStr: String, fTypeStr: String) = {
     for {
-      vType <- parseValueType(vTypeStr).toEither
-      fType <- parseFeatureType(fTypeStr, vType).toEither
+      vType <- parseValueType(vTypeStr).right
+      fType <- parseFeatureType(fTypeStr, vType).right
     } yield (vType, fType)
-  }.validation
+  }
 
   def parseValueType(vTypeStr: String) = vTypeStr.toLowerCase match {
-      case "double" => "DecimalType".success
-      case "int"    => "IntegralType".success
-      case "string" => "StringType".success
-      case _        => s"Unknown value type '$vTypeStr'".failure
+      case "double" => Right("Decimal")
+      case "int"    => Right("Integral")
+      case "string" => Right("Str")
+      case _        => Left(s"Unknown value type '$vTypeStr'")
     }
 
   def parseFeatureType(fTypeStr: String, vType: String) = fTypeStr.toLowerCase match {
       case "continuous" => vType match {
-        case "DecimalType"  => "Continuous".success
-        case "IntegralType" => "Discrete".success
-        case _              => s"Invalid value type 'vType' for continuous feature".failure
+        case "Decimal"  => Right("Continuous")
+        case "Integral" => Right("Discrete")
+        case _          => Left(s"Invalid value type '$vType' for continuous feature")
       }
       case "categorical" => vType match {
-        case "IntegralType" => "Ordinal".success
-        case "StringType"   => "Nominal".success
-        case _              => s"Invalid value type 'vType' for categorical feature".failure
+        case "Integral" => Right("Ordinal")
+        case "Str"      => Right("Nominal")
+        case _          => Left(s"Invalid value type '$vType' for categorical feature")
       }
-      case _ => s"Unknown feature type '$fTypeStr'".failure
+      case _ => Left(s"Unknown feature type '$fTypeStr'")
    }
 
   def toScala(sourceType: String)(metadata: Iterable[Metadata]) = s"""
