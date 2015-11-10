cdd06b3ce534a3cf8a7d5f99910e62b286bc5685
Kristian Domagala <krislistmail+git@gmail.com>
2015-11-10 16:38:03 +1100

    Join aggregators to run in single MR job
    Compose all aggregators from a feature set into a single aggregator that
    accumulates all aggregated values in a single map-reduce job.

--- scalding/src/main/scala/commbank/coppersmith/scalding/FeatureJob.scala
+++ scalding/src/main/scala/commbank/coppersmith/scalding/FeatureJob.scala
@@ -53,29 +53,32 @@ trait SimpleFeatureJobOps {
     }
   }
 
-  // Should be able to take advantage of shapless' tuple support in combination with Aggregator.join
-  // in order to run the aggregators in one pass over the input. Need to consider that features may
-  // have different filter conditions though.
   private def generateAggregate[S](
     features: AggregationFeatureSet[S]
   )(input: TypedPipe[S], ctx: FeatureContext): TypedPipe[(FeatureValue[_], Time)] = {
     val grouped: Grouped[EntityId, S] = input.groupBy(s => features.entity(s))
-    features.aggregationFeatures.map(f =>
-      aggregate(grouped, serialisable(f), ctx)
-    ).foldLeft(TypedPipe.from(List[(FeatureValue[_], Time)]()))(_ ++ _)
-  }
-
-  private def aggregate[S, SV, V <: Value](
-    grouped: Grouped[EntityId, S],
-    feature: SerialisableAggregationFeature[S, SV, V],
-    ctx:     FeatureContext
-  ) = {
-    val name = feature.name
-    val aggregator = composeView(feature.aggregator, feature.view)
-    grouped.aggregate(aggregator).toTypedPipe.collect { case (e, Some(v)) =>
-      (FeatureValue(e, name, v), ctx.generationTime.getMillis)
+    val (joinedAggregator, unjoiner) = join(features.aggregationFeatures.toList.map(serialisable(_)))
+    grouped.aggregate(joinedAggregator).toTypedPipe.flatMap { case (e, v) =>
+      unjoiner.values(e, v).map((_, ctx.generationTime.getMillis))
     }
   }
+
+  // Work-around for problem of TypeTag instances being tied to AggregationFeature and failing at
+  // runtime due to serialisation issues. A more elegant fix would be to try to inline the above
+  // aggregate method into generateAggregate, however, getting the types to line up is non-trivial.
+  // Also takes care of applying the source view, resulting in aggregators from the same set working
+  // on the same source type.
+  private def serialisable[S, SV, V <: Value](
+    feature: AggregationFeature[S, SV, _, V]
+  ): SerialisableAggregationFeature[S] = {
+    val aggregator = composeView(feature.aggregator, feature.view)
+    SerialisableAggregationFeature[S](feature.name, aggregator)
+  }
+
+  case class SerialisableAggregationFeature[S](
+    name:       Name,
+    aggregator: Aggregator[S, _, Option[Value]]
+  )
 
   def composeView[S, SV, B, V <: Value](
     aggregator: Aggregator[SV, B, V],
@@ -90,15 +93,61 @@ trait SimpleFeatureJobOps {
     }
   }
 
-  // Work-around for problem of TypeTag instances being tied to AggregationFeature and failing at
-  // runtime due to serialisation issues. A more elegant fix would be to try to inline the above
-  // aggregate method into generateAggregate, however, getting the types to line up is non-trivial.
-  private def serialisable[S, SV, V <: Value](feature: AggregationFeature[S, SV, _, V]) =
-    SerialisableAggregationFeature[S, SV, V](feature.name, feature.view, feature.aggregator)
+  // Note: Could probably avoid reflection in join() and pattern matching on types in
+  // NextUnjoiner.values() by changing AggregationFeatureSet.aggregationFeatures to be an HList
 
-  case class SerialisableAggregationFeature[S, SV, V <: Value](
-    name:       Name,
-    view:       PartialFunction[S, SV],
-    aggregator: Aggregator[SV, _, V]
-  )
+  /*
+   * Join (compose) aggregators of the form:
+   *
+   *   List(Agg[S, A1, Option[Value1]], Agg[S, A2, Option[Value2]], Agg[S, A3, Option[Value2]], ...)
+   *
+   * to a single aggregator of the form
+   *
+   *   Agg[S, (A1, (A2, (A3, ...))), (Option[Value1], (Option[Value2], (Option[Value3], ...)))]
+   *
+   * and return with an Unjoiner that can deconstruct the resulting values and associate them
+   * back with their original feature name.
+   */
+  def join[S](features: List[SerialisableAggregationFeature[S]]): (Aggregator[S, _, _], Unjoiner) = {
+    features match {
+      case a :: as => {
+        val (agg, remaining) = join(as)
+        (a.aggregator.join(agg).asInstanceOf[Aggregator[S, _, _]], NextUnjoiner(a.name, remaining))
+      }
+      // Dummy aggregator for base case - value will always be ignored by LastUnjoiner
+      case Nil => (Aggregator.const[Option[Value]](None), LastUnjoiner)
+    }
+  }
+
+  /*
+   * Takes values in the form of:
+   *
+   *    (Option[Value1], (Option[Value2], (Option[Value3], ...)))
+   *
+   * from the result of a set of joined aggregation features, and returns a list of
+   * corresponding FeatureValue instances with the original feature name:
+   *
+   *   List(FeatureValue(e, name1, val1), FeatureValue(e, name2, val2), FeatureValue(e, name3, val3), ...)
+   *
+   * `None` values are filtered out of the returned list, as they represent input that that
+   * is filtered completely out as a result of lifting the original aggregator and source
+   * view PartialFunction into the Option MonoidAggregator.
+   */
+  sealed trait Unjoiner {
+    def values(e: EntityId, a: Any): List[FeatureValue[Value]]
+  }
+
+  case class NextUnjoiner(name: Name, remaining: Unjoiner) extends Unjoiner {
+    def values(e: EntityId, a: Any): List[FeatureValue[Value]] = a match {
+      case (Some(v: Value), vs) => FeatureValue(e, name, v) :: remaining.values(e, vs)
+      case (None, vs) => remaining.values(e, vs)
+      // Will only occur if implemenation of values falls out of sync with join (from above).
+      case _ => sys.error("Assumption failed: Wrong shape " + a)
+    }
+  }
+
+  // Marker for recursion base case
+  case object LastUnjoiner extends Unjoiner {
+    def values(e: EntityId, a: Any): List[FeatureValue[Value]] = List()
+  }
 }
