c83b6ab64d74f74b8190f7dd27db3517b27caac6
Konstantinos Servis <knservis@gmail.com>
2016-01-14 09:19:56 +1100

    Added MovieLens example

---
+++ examples/src/main/scala/commbank/coppersmith/examples/MovieLens.scala
@@ -0,0 +1,64 @@
+package commbank.coppersmith.examples.userguide
+
+import au.com.cba.omnia.maestro.api.Maestro.DerivedDecode
+
+import com.twitter.scalding.typed.TypedPipe
+import com.twitter.scalding.{Config, Execution, TypedTsv}
+
+import commbank.coppersmith.Feature.Value.{Decimal, Integral, Str}
+import commbank.coppersmith.Feature.{EntityId, Time}
+import commbank.coppersmith.api._
+import commbank.coppersmith.api.scalding._
+import commbank.coppersmith.examples.thrift.{Movie, Rating}
+import commbank.coppersmith.scalding.FeatureSink
+import commbank.coppersmith.{BoundFeatureSource, FeatureValue}
+
+import org.apache.hadoop.fs.Path
+import org.joda.time.DateTime
+
+case class MovieFeaturesConfig(conf: Config) extends FeatureJobConfig[(Movie, Rating)] {
+  val partitions    = ScaldingDataSource.Partitions.unpartitioned
+  val ratings       = HiveTextSource[Rating, Nothing](new Path("/data/rating"), partitions)
+  val movies        = HiveTextSource[Movie, Nothing](new Path("/data/movie"), partitions)
+
+  override def featureSink: FeatureSink = FlatFeatureSink("/data/output")
+
+  override def featureContext: FeatureContext = ExplicitGenerationTime(new DateTime(2015, 8, 29, 0, 0))
+
+  override def featureSource: BoundFeatureSource[(Movie, Rating), TypedPipe] = MovieFeatures.source.bind(join(movies, ratings))
+}
+
+case class FlatFeatureSink(output: String) extends FeatureSink {
+  override def write(features: TypedPipe[(FeatureValue[_], Time)]): Execution[Unit] = {
+
+    val featurePipe = features.map { case (fv, t) =>
+      val featureValue = (fv.value match {
+        case Integral(v) => v.map(_.toString)
+        case Decimal(v)  => v.map(_.toString)
+        case Str(v)      => v
+      }).getOrElse("")
+      s"${fv.entity}|${fv.name}|${featureValue}"
+    }
+    featurePipe.writeExecution(TypedTsv[String](output)).unit
+  }
+}
+
+object MovieFeatures extends AggregationFeatureSet[(Movie, Rating)]{
+  override def entity(s: (Movie, Rating)): EntityId = s._1.id
+
+  override def aggregationFeatures = List(averageRating)
+
+  override def namespace = "movielens"
+
+  val source = Join[Movie].to[Rating].on(
+    movie   => movie.id,
+    rating  => rating.movieId
+  )
+
+  val select = source.featureSetBuilder(namespace, entity)
+  val averageRating = select(avg(_._2.rating)).asFeature(Continuous, "AVERAGE_MOVIE_RATING", "Average movie rating")
+}
+
+object MovieFeaturesJob extends SimpleFeatureJob {
+  def job = generate(MovieFeaturesConfig(_), MovieFeatures)
+}
---
+++ examples/src/main/thrift/Movie.thrift
@@ -0,0 +1,42 @@
+#   Copyright 2014 Commonwealth Bank of Australia
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+#@namespace scala commbank.coppersmith.examples.thrift
+
+struct Movie {
+  1 	: string  id
+  2 	: string  title
+  3 	: string  date
+  4 	: string  field1
+  5 	: string  field2
+  6 	: string  field3
+  7 	: string  field4
+  8 	: string  field5
+  9 	: string  field6
+  10 	: string  field7
+  11 	: string  field8
+  12 	: string  field9
+  13 	: string  field10
+  14 	: string  field11
+  15 	: string  field12
+  16 	: string  field13
+  17 	: string  field14
+  18 	: string  field15
+  19 	: string  field16
+  20 	: string  field17
+  21 	: string  field18
+  22 	: string  field19
+  23 	: string  field20
+  24 	: string  field21
+ }
