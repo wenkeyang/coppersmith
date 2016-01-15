c9ffe06aa2f8246667e835ffb60c067bf8197ddf
Konstantinos Servis <knservis@gmail.com>
2016-01-15 15:35:51 +1100

    PR feedback: moving FlatFeatureSink out of teh example as it may distract

--- examples/src/main/scala/commbank/coppersmith/examples/MovieLens.scala
+++ examples/src/main/scala/commbank/coppersmith/examples/MovieLens.scala
@@ -2,17 +2,15 @@ package commbank.coppersmith.examples.userguide
 
 import au.com.cba.omnia.maestro.api.Maestro.DerivedDecode
 
-import com.twitter.scalding.typed.TypedPipe
-import com.twitter.scalding.{Config, Execution, TypedTsv}
-
-import commbank.coppersmith.Feature.Value.{Decimal, Integral, Str}
-import commbank.coppersmith.Feature.{EntityId, Time}
+import commbank.coppersmith.Feature.EntityId
 import commbank.coppersmith.api._
 import commbank.coppersmith.api.scalding._
 import commbank.coppersmith.examples.thrift.{Movie, Rating}
+import commbank.coppersmith.BoundFeatureSource
 import commbank.coppersmith.scalding.FeatureSink
-import commbank.coppersmith.{BoundFeatureSource, FeatureValue}
 
+import com.twitter.scalding.typed.TypedPipe
+import com.twitter.scalding.Config
 import org.apache.hadoop.fs.Path
 import org.joda.time.DateTime
 
@@ -27,22 +25,6 @@ case class MovieFeaturesConfig(conf: Config) extends FeatureJobConfig[(Movie, Ra
 
   override def featureSource: BoundFeatureSource[(Movie, Rating), TypedPipe] = MovieFeatures.source.bind(join(movies, ratings))
 }
-
-case class FlatFeatureSink(output: String) extends FeatureSink {
-  override def write(features: TypedPipe[(FeatureValue[_], Time)]): Execution[Unit] = {
-
-    val featurePipe = features.map { case (fv, t) =>
-      val featureValue = (fv.value match {
-        case Integral(v) => v.map(_.toString)
-        case Decimal(v)  => v.map(_.toString)
-        case Str(v)      => v
-      }).getOrElse("")
-      s"${fv.entity}|${fv.name}|${featureValue}"
-    }
-    featurePipe.writeExecution(TypedTsv[String](output)).unit
-  }
-}
-
 object MovieFeatures extends AggregationFeatureSet[(Movie, Rating)]{
   override def entity(s: (Movie, Rating)): EntityId = s._1.title
 
